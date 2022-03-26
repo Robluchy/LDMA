f843f56fb7e3592e8a81c26be22f609198bfdc16
Robluchy <Dionny75@gmail.com>
2022-03-26 17:44:42 +0100

    jelow

---
+++ js/juego.js
@@ -0,0 +1,18 @@
+let palabra = "teletrabajo";
+let severyn = "border-solid	border-b-4 border_black";
+let numIntentos = 7;
+let listaPalabras = ['classe', 'silla', 'imagen', 'orangutan'];
+
+
+
+
+function letra(letras) {
+    for (var i = 0 ; i < palabra.length; i++);
+        if (letras.innerHtml == palabra[i]) {
+        }
+}
+
+
+$(document).ready(function () {
+    
+});
