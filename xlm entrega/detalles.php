e7447ee01bbd423af1963d846773a59695e904c5
Robluchy <Dionny75@gmail.com>
2022-04-16 10:14:06 +0200

    a

--- xlm entrega/detalles.php
+++ xlm entrega/detalles.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>Document</title>
 </head>
 <body>
     <?php 
+    $id=$_GET['id'];
+    $xml = simplexml_load_file('http://www.aemet.es/xml/municipios/localidad_'.$id.'.xml');
+    $nombre = $xml->xpath('//nombre');
     
-    $xml = simplexml_load_file('');
     ?>
 </body>
 </html>
