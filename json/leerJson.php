fa7a68bfe05390a07a23788870ddbd2c01e56612
Robluchy <Dionny75@gmail.com>
2022-05-21 13:33:57 +0200

    a

--- json/leerJson.php
+++ json/leerJson.php
@@ -50,6 +50,10 @@
             echo "DNI " .$user["login"]["uuid"] . "<br>";
         }
         ?>
-
+    <footer class="fixed w-full bottom-0  bg-gray-900 text-center lg:text-left">
+      
+      <div class="text-white text-center p-4">Dionny © 2022 Copyright</div>
+  
+  </footer>
 </body>
 </html>
